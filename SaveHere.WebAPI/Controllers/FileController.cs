08ed48b363d75f01fe48c591894a5be6c5a461bb
Ehsan Gudarzi <ehsan.gdrzi@gmail.com>
2024-03-21 20:20:33 +0330

    Add project files.

---
+++ SaveHere.WebAPI/Controllers/FileController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SaveHere.WebAPI.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class FileController : ControllerBase
+  {
+    private readonly HttpClient _httpClient;
+
+    public FileController(HttpClient httpClient)
+    {
+      _httpClient = httpClient;
+    }
+
+    [HttpPost("download")]
+    public async Task<IActionResult> DownloadFile([FromBody] string url)
+    {
+      var fileName = Path.GetFileName(url);
+      var localFilePath = Path.Combine("/app/downloads", fileName);
+
+      await _httpClient.GetAsync(url).ContinueWith((task) =>
+      {
+        if (task.IsFaulted || task.IsCanceled) return;
+
+        using var stream = new FileStream(localFilePath, FileMode.Create, FileAccess.Write);
+        task.Result.Content.CopyToAsync(stream);
+      });
+
+      return Ok($"/files/{fileName}");
+    }
+
+  }
+}
---
+++ SaveHere.WebAPI/Program.cs
@@ -0,0 +1,33 @@
+
+namespace SaveHere.WebAPI;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddScoped<HttpClient>();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+
+    app.MapControllers();
+
+    app.Run();
+  }
+}
