82f4454f5bfc9dd8278ed75235425c1e977d4bf6
Ehsan Gudarzi <ehsan.gdrzi@gmail.com>
2024-03-21 23:21:08 +0330

    testing new method of writing to files in backend

--- SaveHere.WebAPI/Controllers/FileController.cs
+++ SaveHere.WebAPI/Controllers/FileController.cs
@@ -24,7 +24,7 @@ namespace SaveHere.WebAPI.Controllers
       {
         if (task.IsFaulted || task.IsCanceled) return;
 
-        using var stream = new FileStream(localFilePath, FileMode.Create, FileAccess.Write);
+        using var stream = new FileStream(localFilePath, FileMode.CreateNew);
         task.Result.Content.CopyToAsync(stream);
       });
 
