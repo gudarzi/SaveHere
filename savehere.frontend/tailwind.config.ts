ffb47f79c36205870cf8f7695e7d6166ac765131
Ehsan Gudarzi <ehsan.gdrzi@gmail.com>
2024-03-24 23:15:59 +0330

    Cleanup frontend app

---
+++ savehere.frontend/tailwind.config.ts
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                myCustomBlue: '#0070f3',
+                myCustomRed: '#ff2d55',
+                myCustomGreen: '#2dd4bf',
+            },
+        },
+    },
+    plugins: [],
+}
--- savehere.frontend/vite.config.ts
+++ savehere.frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +9,9 @@ export default defineConfig({
     outDir: '../frontend',
     emptyOutDir: true,
   },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  }
 })
