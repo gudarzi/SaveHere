7855e70a6095cff4ae1db5ecc659264d16fca170
Clément Campagna <clement.campagna@gmail.com>
2024-05-14 22:33:19 +0200

    Fix for downloading large files + new progress report field
    Downloading large files shouldn't  be a problem anymore (does not hang), and I have also added a field named 'ProgressPercentage' that can read to implement a progress bar on the front-end.

--- SaveHere.WebAPI/Controllers/FileController.cs
+++ SaveHere.WebAPI/Controllers/FileController.cs
@@ -24,34 +24,58 @@ public class FileController : ControllerBase
 
     var fileName = Path.GetFileName(System.Web.HttpUtility.UrlDecode(url));
 
-    await _httpClient.GetAsync(url).ContinueWith(async (task) =>
+    await _httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead).ContinueWith(async (task) =>
     {
-      if (task.IsFaulted || task.IsCanceled) return;
+        if (task.IsFaulted || task.IsCanceled) return;
 
-      if (!useUrlForFilename)
-      {
-        var contentDisposition = task.Result.Content.Headers.ContentDisposition;
-
-        if (contentDisposition != null)
+		if (!useUrlForFilename)
         {
-          if (!string.IsNullOrEmpty(contentDisposition.FileNameStar)) fileName = System.Web.HttpUtility.UrlDecode(contentDisposition.FileNameStar.Replace("\"", ""));
-          else if (!string.IsNullOrEmpty(contentDisposition.FileName)) fileName = System.Web.HttpUtility.UrlDecode(contentDisposition.FileName.Replace("\"", ""));
+            var contentDisposition = task.Result.Content.Headers.ContentDisposition;
+
+            if (contentDisposition != null)
+            {
+                if (!string.IsNullOrEmpty(contentDisposition.FileNameStar)) fileName = System.Web.HttpUtility.UrlDecode(contentDisposition.FileNameStar.Replace("\"", ""));
+                else if (!string.IsNullOrEmpty(contentDisposition.FileName)) fileName = System.Web.HttpUtility.UrlDecode(contentDisposition.FileName.Replace("\"", ""));
+            }
         }
-      }
 
-      fileName = string.Join("_", fileName.Split(Path.GetInvalidFileNameChars(), StringSplitOptions.RemoveEmptyEntries)).TrimEnd('.');
+        fileName = string.Join("_", fileName.Split(Path.GetInvalidFileNameChars(), StringSplitOptions.RemoveEmptyEntries)).TrimEnd('.');
 
-      if (string.IsNullOrWhiteSpace(fileName)) fileName = "unnamed_" + DateTime.Now.ToString("yyyyMMddHHmmss");
+        if (string.IsNullOrWhiteSpace(fileName)) fileName = "unnamed_" + DateTime.Now.ToString("yyyyMMddHHmmss");
 
-      var localFilePath = Path.Combine("/app/downloads", fileName);
+        var localFilePath = Path.Combine("/app/downloads", fileName);
 
-      using var stream = new FileStream(localFilePath, FileMode.Create, FileAccess.Write);
-      await task.Result.Content.CopyToAsync(stream);
+        using(var download = await task.Result.Content.ReadAsStreamAsync())
+        {
+			using var stream = new FileStream(localFilePath, FileMode.Create, FileAccess.Write);
+            var contentLength = task.Result.Content.Headers.ContentLength;
 
-      // Fixing file permissions on linux
-      if (System.OperatingSystem.IsLinux()) System.IO.File.SetUnixFileMode(localFilePath,
-        UnixFileMode.UserRead | UnixFileMode.GroupRead | UnixFileMode.OtherRead | UnixFileMode.UserWrite | UnixFileMode.GroupWrite | UnixFileMode.OtherWrite | UnixFileMode.UserExecute | UnixFileMode.GroupExecute | UnixFileMode.OtherExecute);
-    });
+			// Ignore progress reporting when the ContentLength's header is not available
+			if (!contentLength.HasValue)
+            {
+				request.ProgressPercentage = 0;
+				await download.CopyToAsync(stream).ConfigureAwait(false);
+				request.ProgressPercentage = 100;
+			}
+            else
+            {
+                var buffer = new byte[81920]; // default buffer size used by Microsoft's CopyTo method in Stream
+				long totalBytesRead = 0;
+                int bytesRead;
+
+                while ((bytesRead = await download.ReadAsync(buffer, 0, buffer.Length).ConfigureAwait(false)) != 0)
+                {
+                    await stream.WriteAsync(buffer, 0, bytesRead).ConfigureAwait(false);
+                    totalBytesRead += bytesRead;
+                    request.ProgressPercentage = (int)Math.Floor(totalBytesRead / (float)contentLength * 100.0);
+				}
+            }
+		}
+
+        // Fixing file permissions on linux
+        if (System.OperatingSystem.IsLinux()) System.IO.File.SetUnixFileMode(localFilePath,
+          UnixFileMode.UserRead | UnixFileMode.GroupRead | UnixFileMode.OtherRead | UnixFileMode.UserWrite | UnixFileMode.GroupWrite | UnixFileMode.OtherWrite | UnixFileMode.UserExecute | UnixFileMode.GroupExecute | UnixFileMode.OtherExecute);
+    }).ConfigureAwait(false);
 
     return Ok($"/files/{fileName}");
   }
--- SaveHere.WebAPI/DTOs/DownloadFileRequestDTO.cs
+++ SaveHere.WebAPI/DTOs/DownloadFileRequestDTO.cs
@@ -3,5 +3,9 @@
 public class DownloadFileRequestDTO
 {
   public string? Url { get; set; }
+
   public bool? UseUrlForFilename { get; set; } // making it nullable to allow for omission
+
+  [System.Text.Json.Serialization.JsonIgnore]
+  public int ProgressPercentage { get; set; } = 0; // goes from 0 to 100 but will stay at 0 then go to 100 if ContentLength is not available
 }
